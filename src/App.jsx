6897a5777bf6c9e477c60f7046e485a424fca413
RosaBen <benchabane.rosa@outlook.com>
2026-05-14 03:15:10 +0200

    added props for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
 
+import {data} from "./assets/scripts/data"
+
 function App() {
-  
 
   return (
     <>
       <Header/>
       <div className="container">
-        <Entry/>
+        <Entry props={data[0]}/>
       </div>
       
       {/* </>
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,22 +1,23 @@
-import japan from "../assets/images/mountfuji.png"
+
+// import japan from "../assets/images/mountfuji.png"
 import marker from "../assets/images/marker.svg"
-export default function Entry(){
+export default function Entry({props}){
   return(
     <section className="card">
-      <img src={japan} alt="Mount Fuji in Japan" className="scenery-img"/>
+      <img src={props.img.src} alt={props.img.alt} className="scenery-img"/>
       <article className="description">
-        <h2>Mount Fuji</h2>
+        <h2>{props.title}</h2>
         <div className="localisation">
           <img src={marker} alt="marker for localisation" />
-          <h3>Japan</h3>
-          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+          <h3>{props.country}</h3>
+          <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
         </div>
         <div className="dates">
-          <time datetime="2023-01-12">12 Jan, 2023</time>
+          <time dateTime="2023-01-12">{props.dates.begin}</time>
           <span> - </span>
-          <time datetime="2023-01-24">24 Jan, 2023</time>
+          <time dateTime="2023-01-24">{props.dates.end}</time>
         </div>
-        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
+        <p>{props.text}</p>
       </article>
     </section>
   )
